1edf6437a12c049a66094e562833927bc1a33ce1
Paltinych <pgumennov@yandex.ru>
2023-01-22 16:27:25 +0300

    rewritten Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
